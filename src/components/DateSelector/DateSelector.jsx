539a79d1757c3bae7d7adb5c961dbeae795d496f
funversuta <funversuta@gmail.com>
2023-08-28 23:39:32 +0700

    fixes project

--- src/components/DateSelector/DateSelector.jsx
+++ src/components/DateSelector/DateSelector.jsx
@@ -1,46 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import CustomNouislider from "../NoUiSlider/NoUiSlider";
-import styles from "./DateSelector.module.scss";
+import styles from "./DateSelector.module.css";
 
-let minValue = "2014";
-let maxValue = "2021";
-const defaultMinValue = "2015, 5";
-const defaultMaxValue = "2016, 2";
+const minValue = new Date(2015, 1, 0);
+const maxValue = new Date(2017, 0, 1);
+const defaultMinValue = new Date(2015, 6, 0);
+const defaultMaxValue = new Date(2016, 2, 1);
 
 const DateSelector = () => {
-  const [YearBtn, setYearBtn] = useState(true);
-  const [MonthBtn, setMonthBtn] = useState(false);
+  const [yearBtn, setYearBtn] = useState(true);
 
   const handleYearsClick = () => {
     setYearBtn(true);
-    setMonthBtn(false);
   };
 
   const handleMonthsClick = () => {
-    setMonthBtn(true);
     setYearBtn(false);
   };
 
-  const getDatepoints = (str) => {
+  const getDatePoints = (str) => {
     return new Date(str).getTime();
   };
-
-  useEffect(() => {
-    if (YearBtn) {
-      minValue = "2015";
-      maxValue = "2017";
-    } else {
-      minValue = "2014";
-      maxValue = "2021";
-    }
-  }, [YearBtn]);
 
   return (
     <div className={styles.DateSelector}>
       <div className={styles.DateSelectorButtons}>
         <button
           className={`${styles.DateSelectorButton} ${
-            YearBtn ? styles.DateSelectorButtonActive : ""
+            yearBtn ? styles.DateSelectorButtonActive : ""
           } `}
           onClick={handleYearsClick}
         >
@@ -48,7 +35,7 @@ const DateSelector = () => {
         </button>
         <button
           className={`${styles.DateSelectorButton} ${
-            MonthBtn ? styles.DateSelectorButtonActive : ""
+            !yearBtn ? styles.DateSelectorButtonActive : ""
           } `}
           onClick={handleMonthsClick}
         >
@@ -57,11 +44,11 @@ const DateSelector = () => {
       </div>
       <div className={styles.DateSelectorRangeContainer}>
         <CustomNouislider
-          isYearBtn={YearBtn}
-          minValue={getDatepoints(minValue)}
-          maxValue={getDatepoints(maxValue)}
-          defaultMin={getDatepoints(defaultMinValue)}
-          defaultMax={getDatepoints(defaultMaxValue)}
+          isYearBtn={yearBtn}
+          minValue={getDatePoints(minValue)}
+          maxValue={getDatePoints(maxValue)}
+          defaultMin={getDatePoints(defaultMinValue)}
+          defaultMax={getDatePoints(defaultMaxValue)}
         />
       </div>
     </div>
--- src/components/NoUiSlider/NoUiSlider.jsx
+++ src/components/NoUiSlider/NoUiSlider.jsx
@@ -2,82 +2,83 @@ import Nouislider from "nouislider-react";
 import { useEffect, useState } from "react";
 import "./NoUiSlider.css";
 
-const CustomNouislider = ({
-  isYearBtn,
-  minValue,
-  maxValue,
-  defaultMin,
-  defaultMax,
-}) => {
-  const [AlternativeGap, setAlternativeGap] = useState(false);
+const CustomNouislider = (props) => {
+  const { isYearBtn, minValue, maxValue, defaultMin, defaultMax } = props;
+  const step = 1000 * 60 * 60 * 24 * 30.75;
+  const tooLongScale = 94694400000;
+  const [alternativeGap, setAlternativeGap] = useState(false);
 
-  const DateFormat = new Intl.DateTimeFormat("ru", {
+  const dateFormat = new Intl.DateTimeFormat("ru", {
     year: "numeric",
     month: "long",
   });
 
   useEffect(() => {
-    if (maxValue - minValue > 94694400000) {
-      setAlternativeGap(true);
-    } else {
-      setAlternativeGap(false);
-    }
-  }, [maxValue, minValue]);
+    maxValue - minValue > tooLongScale
+      ? setAlternativeGap(true)
+      : setAlternativeGap(false);
+  }, [maxValue, minValue, isYearBtn]);
 
   const formatDateTooltip = (value) => {
-    return DateFormat.format(value).replace(/\s*г\./, "");
+    return dateFormat.format(value).replace(/\s*г\./, "");
   };
 
   const formatPips = (value) => {
     const date = new Date(value);
     const year = date.getFullYear();
 
-    if (AlternativeGap) {
-      const shortingMonth = DateFormat.format(value).substring(0, 3);
+    if (alternativeGap && !isYearBtn) {
+      const shortingMonth = dateFormat.format(value).substring(0, 3);
       return shortingMonth === "янв"
         ? year
         : shortingMonth.replace(/[\d\W]+/g, ".");
     }
-    const month = DateFormat.format(value).substring(0, 3);
+
+    if (alternativeGap && isYearBtn) {
+      return year === new Date(minValue).getFullYear() ||
+        year === new Date(maxValue).getFullYear()
+        ? year
+        : year.toString().replace(/[\d\W]+/g, ".");
+    }
+    const month = dateFormat.format(value).substring(0, 3);
     return month === "янв" ? year : month;
   };
 
   const filterYearPips = (value) => {
     const date = new Date(value);
     const year = date.getFullYear();
-    const month = DateFormat.format(value).substring(0, 3);
+    const month = dateFormat.format(value).substring(0, 3);
 
-    if (AlternativeGap && month === "янв") {
+    if (alternativeGap && month === "янв") {
       return year % 0 ? -1 : 1;
     }
     return month === "янв" ? 1 : -1;
   };
 
   return (
-    <>
-      <Nouislider
-        key={[isYearBtn, AlternativeGap]}
-        range={{ min: minValue, max: maxValue }}
-        step={1000 * 60 * 60 * 24 * 30.75}
-        start={[defaultMin, defaultMax]}
-        tooltips={[
-          {
-            to: formatDateTooltip,
-          },
-          {
-            to: formatDateTooltip,
-          },
-        ]}
-        pips={{
-          mode: "steps",
-          filter: isYearBtn ? filterYearPips : null,
-          format: {
-            to: formatPips,
-          },
-        }}
-        connect
-      />
-    </>
+    <Nouislider
+      key={[isYearBtn, alternativeGap]}
+      range={{ min: minValue, max: maxValue }}
+      step={step}
+      start={[defaultMin, defaultMax]}
+      tooltips={[
+        {
+          to: formatDateTooltip,
+        },
+        {
+          to: formatDateTooltip,
+        },
+      ]}
+      pips={{
+        mode: "steps",
+        density: 25,
+        filter: isYearBtn ? filterYearPips : null,
+        format: {
+          to: formatPips,
+        },
+      }}
+      connect
+    />
   );
 };
 
